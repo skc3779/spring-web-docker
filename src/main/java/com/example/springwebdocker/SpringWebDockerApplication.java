8d916da620a0190506b0b5345a5e056e52909255
kangchun.seo <skc3779@gmail.com>
2023-07-30 15:27:33 +0900

    프로그램 수정.

---
+++ src/main/java/com/example/springwebdocker/SpringWebDockerApplication.java
@@ -0,0 +1,13 @@
+package com.example.springwebdocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebDockerApplication.class, args);
+	}
+
+}
